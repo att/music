6b9576348b67655216dcc5cd72cff76c01cbeae8
Balasubramanian, Bharath (bb536c) <bharathb@research.att.com>
2017-07-23 10:46:10 -0400

    Add updated scripts to music benchmarks

--- music-benchmarks/scripts/runJmeterTest.sh
+++ music-benchmarks/scripts/runJmeterTest.sh
@@ -8,22 +8,23 @@
 # results. 
 
 
-eval "$(jq -r '@sh "op_type=\(.op_type) "' music_bm.json)"
 num_threads=$1
 num_trials=$2
+op_type=$(head -n 1 music_bm.txt)
+
 
 echo operation_type = $op_type
 echo num_threads = $num_threads
 echo num_trials = $num_trials
 current_date_time="`date +%d_%H_%M`";
-folder_name=$op_type$current_date_time
+folder_name=$op_type
 echo results stored in folder: results/$folder_name
 
 #start running the trials
 
 for (( i=1; i<=$num_trials; i++ ))
 do  
-	result_file_name=$op_type$i.mrst
+	result_file_name=run_$current_date_time.trial_$i.txt
   echo -----Trial $i started, results in file results/$folder_name/$result_file_name---
 	./jmeter -Jusers=$num_threads -n -t testplans/music.jmx -l results/$folder_name/$result_file_name
 done

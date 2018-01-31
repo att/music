0f3c8ca2097cdf480deecaf8b946baa8ad53c22a
Brendan <bt054f@att.com>
2018-01-31 10:47:12 -0500

    remove early exit of restartHalIfDead script

--- recipes/hal/sampleApp/restartHalIfDead.sh
+++ recipes/hal/sampleApp/restartHalIfDead.sh
@@ -11,8 +11,6 @@ if [[ "$#" -ge 2 && ${2//[-]} == p* ]]; then
     echo "Start HAL in passive mode"
 fi
  
-exit 0
-
 dir=$PWD
 ps aux > $dir/HalLog$1.out
 halId=`grep "hal.jar $1" $dir/HalLog$1.out | awk '{ print $2 }'`

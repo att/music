1aca930ce8e2b849c657eed1f2de1ef3197ee32c
Brendan <bt054f@att.com>
2018-02-09 13:08:31 -0500

    update passive script to check site status and health

--- recipes/hal/sampleApp/ensureSdncStandby.sh
+++ recipes/hal/sampleApp/ensureSdncStandby.sh
@@ -4,6 +4,13 @@
 clusterStatus=$( /opt/sdnc/sdnc.cluster )
 
 if [ "ACTIVE" = "$clusterStatus" ];then
+  # check that standby cluster is healthy
+  health=$( /opt/sdnc/sdnc.monitor )
+  if [ "FAILURE" = "$health" ];then
+    echo "Backup site is unhealthy - can't accept traffic!"
+    exit 1
+  fi
+
   # assume transient error as other side transitions to ACTIVE
   echo "Cluster is ACTIVE but HAL wants STANDBY! Panic!"
   exit 0

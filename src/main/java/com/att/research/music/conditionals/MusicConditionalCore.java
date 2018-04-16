d2708ca2a418d3d02d3f95de953ea87d0ac04b90
srupanek <srupane@gmail.com>
2018-04-16 14:08:12 -0400

    updated war file in the root folder

--- src/main/java/com/att/research/music/conditionals/MusicConditionalCore.java
+++ src/main/java/com/att/research/music/conditionals/MusicConditionalCore.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
+import org.codehaus.jettison.json.JSONObject;
 
 import com.att.research.music.datastore.MusicDataStore;
 import com.att.research.music.lockingservice.MusicLockState;
@@ -49,6 +50,7 @@ public class MusicConditionalCore {
 				return lockAcqResult;
 			}
 		} catch (Exception e) {
+			logger.error(e.getMessage());
 			MusicCore.destroyLockRef(lockId);
 			return new ReturnType(ResultType.FAILURE, e.getMessage());
 		}
@@ -62,7 +64,12 @@ public class MusicConditionalCore {
 			MusicLockState mls = MusicCore.getLockingServiceHandle()
 					.getLockState(keyspaceName + "." + tableName + "." + primaryKey);
 			if (mls.getLockHolder().equals(lockId) == true) {
-				results = MusicCore.getDSHandle().executeCriticalGet(query.get("select"));
+				try {
+					results = MusicCore.getDSHandle().executeCriticalGet(query.get("select"));
+				} catch (Exception e) {
+					logger.error(e.getMessage());
+					return new ReturnType(ResultType.FAILURE, e.getMessage());
+				}
 				if (results.all().isEmpty()) {
 					MusicCore.getDSHandle().executePut(query.get("insert"), "critical");
 					return new ReturnType(ResultType.SUCCESS, "insert");
@@ -76,6 +83,7 @@ public class MusicConditionalCore {
 						"Cannot perform operation since you are the not the lock holder");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
+			logger.error(e.getMessage());
 			StringWriter sw = new StringWriter();
 			e.printStackTrace(new PrintWriter(sw));
 			String exceptionAsString = sw.toString();
@@ -86,8 +94,9 @@ public class MusicConditionalCore {
 
 	}
 
-	public static ReturnType conditionalUpdate(Map<String, String> query, String tableName, String keySpace,
-			String primaryKey, Map<String, String> changeOfStatus,String cascadeColumnName,String primaryKeyName,String planId) {
+	public static ResponseObject conditionalUpdate(Map<String, String> query, String tableName, String keySpace,
+			String primaryKey, Map<String, String> changeOfStatus, String cascadeColumnName, String primaryKeyName,
+			String planId) {
 
 		long start = System.currentTimeMillis();
 		String key = keySpace + "." + tableName + "." + primaryKey;
@@ -97,8 +106,8 @@ public class MusicConditionalCore {
 		try {
 			if (lockAcqResult.getResult().equals(ResultType.SUCCESS)) {
 				logger.info("acquired lock with id " + lockId);
-				ReturnType criticalPutResult = conditionalUpdateCriticalPut(keySpace, tableName, primaryKey, query,
-						lockId, changeOfStatus, cascadeColumnName,primaryKeyName,planId);
+				ResponseObject criticalPutResult = conditionalUpdateCriticalPut(keySpace, tableName, primaryKey, query,
+						lockId, changeOfStatus, cascadeColumnName, primaryKeyName, planId);
 				boolean voluntaryRelease = true;
 				MusicCore.releaseLock(lockId, voluntaryRelease);
 				long end = System.currentTimeMillis();
@@ -107,154 +116,128 @@ public class MusicConditionalCore {
 			} else {
 				logger.info("unable to acquire lock, id " + lockId);
 				MusicCore.destroyLockRef(lockId);
-				return lockAcqResult;
+				return new ResponseObject(ResultType.FAILURE, lockAcqResult.getMessage());
 			}
 		} catch (Exception e) {
+			logger.error(e.getMessage());
 			MusicCore.destroyLockRef(lockId);
-			return new ReturnType(ResultType.FAILURE, e.getMessage());
+			return new ResponseObject(ResultType.FAILURE, e.getMessage());
 		}
 	}
 
-	public static ReturnType conditionalUpdateCriticalPut(String keyspaceName, String tableName, String primaryKey,
-			Map<String, String> query, String lockId,Map<String, String> changeOfStatus,String cascadeColumnName,String primaryKeyname,String planId) {
-		//have an object that contains all the fields instead of sending way too many params.
-		long start = System.currentTimeMillis();
+	public static ResponseObject conditionalUpdateCriticalPut(String keyspaceName, String tableName, String primaryKey,
+			Map<String, String> query, String lockId, Map<String, String> changeOfStatus, String cascadeColumnName,
+			String primaryKeyname, String planId) {
+		// have an object that contains all the fields instead of sending way too many
+		// params.
 		ResultSet results = null;
 		Row row = null;
-		Map<String,String> updatedValues = new HashMap<String,String>();
+		Map<String, String> updatedValues = new HashMap<String, String>();
 		String updateQuery = "";
 		try {
 			MusicLockState mls = MusicCore.getLockingServiceHandle()
 					.getLockState(keyspaceName + "." + tableName + "." + primaryKey);
 			if (mls.getLockHolder().equals(lockId) == true) {
-				results = MusicCore.getDSHandle().executeCriticalGet(query.get("select"));
+				try {
+					results = MusicCore.getDSHandle().executeCriticalGet(query.get("select"));
+				} catch (Exception e) {
+					return new ResponseObject(ResultType.FAILURE, e.getMessage());
+				}
 				row = results.one();
 				if (row != null) {
-					if(planId == null || planId.isEmpty() || planId.length()==0)
-					updatedValues = cascadeColumnUpdateAll(row, changeOfStatus, cascadeColumnName);//remove this function.just for testing
-					else
-					updatedValues = cascadeColumnUpdateSpecific(row, changeOfStatus, cascadeColumnName,planId);
+					if (planId != null || !planId.isEmpty() || planId.length() != 0)
+						updatedValues = cascadeColumnUpdateSpecific(row, changeOfStatus, cascadeColumnName, planId);
 					ColumnDefinitions colInfo = row.getColumnDefinitions();
 					DataType colType = colInfo.getType(cascadeColumnName);
-					updateQuery = getUpdateQuery(updatedValues, tableName, keyspaceName, primaryKey, cascadeColumnName, colType, primaryKeyname);
-					MusicCore.getDSHandle().executePut(updateQuery, "critical");
-					return new ReturnType(ResultType.SUCCESS, updatedValues.toString());
-					
-				} else {
-				
-					return new ReturnType(ResultType.FAILURE, "update failed");
+					updateQuery = getUpdateQuery(updatedValues, tableName, keyspaceName, primaryKey, cascadeColumnName,
+							colType, primaryKeyname);
+					try {
+						MusicCore.getDSHandle().executePut(updateQuery, "critical");
+					} catch (Exception e) {
+						return new ResponseObject(ResultType.FAILURE, "update failed");
+					}
 				}
+				else {
+					return new ResponseObject(ResultType.FAILURE,"No data found");
+				}
+				results = MusicCore.getDSHandle().executeCriticalGet(query.get("select"));
+				return new ResponseObject(ResultType.SUCCESS, results);
 			} else
-				return new ReturnType(ResultType.FAILURE,
+				return new ResponseObject(ResultType.FAILURE,
 						"Cannot perform operation since you are the not the lock holder");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			StringWriter sw = new StringWriter();
 			e.printStackTrace(new PrintWriter(sw));
 			String exceptionAsString = sw.toString();
-			return new ReturnType(ResultType.FAILURE,
+			return new ResponseObject(ResultType.FAILURE,
 					"Exception thrown while doing the critical put, check sanctity of the row/conditions:\n"
 							+ exceptionAsString);
 		}
 
 	}
-	//TODO
-	//remove this function. just for test
+
 	@SuppressWarnings("unchecked")
-	public static Map<String,String> cascadeColumnUpdateAll (Row row,Map<String,String>changeOfStatus,String cascadeColumnName) {
-		
+	public static Map<String, String> cascadeColumnUpdateSpecific(Row row, Map<String, String> changeOfStatus,
+			String cascadeColumnName, String planId) {
+
 		ColumnDefinitions colInfo = row.getColumnDefinitions();
 		DataType colType = colInfo.getType(cascadeColumnName);
-		Map<String,String> values =new HashMap<>();
+		Map<String, String> values = new HashMap<>();
 		Object columnValue = getColValue(row, cascadeColumnName, colType);
-		
-		Map<String,String> finalValues =new HashMap<>();
-	    values  =  (Map<String, String>) columnValue;
-	    Map<String,String> keyValuePair = new HashMap<>();
-	    for(String s:values.keySet()) {
-	    	String temp = values.get(s);
-	    	String temp2 = temp.replaceAll("\\{", "").replaceAll("\\}", "");
-	    	String[] elements = temp2.split(",");
-	    	for(String str:elements) {
-	    		String[] keyValue = str.split("=");
-	    		for(String key : changeOfStatus.keySet()) {
-	    			
-	    			if(keyValue[1].equalsIgnoreCase(key)) {
-	    				keyValue[1] = changeOfStatus.get(key);
-	    			}
-	    			String tempKeytwo = keyValue[0].replaceAll("\\s", "");
-	    			if(tempKeytwo.equalsIgnoreCase(key)) {
-	    				keyValue[1] = changeOfStatus.get(key);
-	    			}
-	    		}
-	    		String finalStatusKey = keyValue[0].replaceAll("\\s", "");
-	    		String finalStatusValue = keyValue[1].replaceAll("\\s", "");
-	    		keyValuePair.put(finalStatusKey, finalStatusValue);
-	    		finalValues.put(s, keyValuePair.toString());
-	    	}
-	    }
-	    return finalValues;
-		
-		
+
+		Map<String, String> finalValues = new HashMap<>();
+		values = (Map<String, String>) columnValue;
+		if (values.keySet().contains(planId)) {
+			String valueString = values.get(planId);
+			String tempValueString = valueString.replaceAll("\\{", "").replaceAll("\"", "").replaceAll("\\}", "");
+			String[] elements = tempValueString.split(",");
+			for (String str : elements) {
+				String[] keyValue = str.split(":");
+				if ((changeOfStatus.keySet().contains(keyValue[0].replaceAll("\\s", ""))))
+				keyValue[1] = changeOfStatus.get(keyValue[0].replaceAll("\\s", ""));
+				finalValues.put(keyValue[0], keyValue[1]);
+			}
+		}
+		values.remove(planId);
+		JSONObject json = new JSONObject(finalValues);
+		values.put(planId, json.toString());
+		return values;
+
 	}
-	
-public static Map<String,String> cascadeColumnUpdateSpecific (Row row,Map<String,String>changeOfStatus,String cascadeColumnName,String planId) {
-		
-		ColumnDefinitions colInfo = row.getColumnDefinitions();
-		DataType colType = colInfo.getType(cascadeColumnName);
-		Map<String,String> values =new HashMap<>();
-		Object columnValue = getColValue(row, cascadeColumnName, colType);
-		
-		Map<String,String> finalValues =new HashMap<>();
-	    values  =  (Map<String, String>) columnValue;
-	    Map<String,String> keyValuePair = new HashMap<>();
-	    if(values.keySet().contains(planId)) {
-	    	String valueString = values.get(planId);
-	    	String tempValueString = valueString.replaceAll("\\{", "").replaceAll("\\}", "");
-	    	String[] elements = tempValueString.split(",");
-	    	for(String str : elements) {
-	    		String[] keyValue = str.split("=");
-	    		if((changeOfStatus.keySet().contains(keyValue[0].replaceAll("\\s", ""))));
-	    		     keyValue[1] = changeOfStatus.get(keyValue[0].replaceAll("\\s", ""));
-	    		finalValues.put(keyValue[0], keyValue[1]);     
-	    	}
-	    }
-	    values.remove(planId);
-	    values.put(planId, finalValues.toString());
-	    return values;
-		
-		
-	}
-	
-	public static String getUpdateQuery(Map<String,String> values,String tableName,String keySpace,String primaryKey,String columnName,DataType colType,String PrimaryKeyName) {
+
+	public static String getUpdateQuery(Map<String, String> values, String tableName, String keySpace,
+			String primaryKey, String columnName, DataType colType, String PrimaryKeyName) {
 		String formatedValue = MusicCore.convertToCQLDataType(colType, values);
-		String query = "Update "+keySpace+"."+tableName+ " SET "+columnName+ " = "+ formatedValue +" WHERE "+ PrimaryKeyName+" = '"+ primaryKey+"';";
-		
+		String vectorTs = "'"+Thread.currentThread().getId()+System.currentTimeMillis()+"'";
+		String query = "Update " + keySpace + "." + tableName + " SET " + columnName + " = " + formatedValue+ ", vector_ts = "+vectorTs + " WHERE "
+				+ PrimaryKeyName + " = '" + primaryKey + "';";
+
 		return query;
-		
+
 	}
-	
-	public static  Object getColValue(Row row, String colName, DataType colType){	
-		switch(colType.getName()){
-		case VARCHAR: 
+
+	public static Object getColValue(Row row, String colName, DataType colType) {
+		switch (colType.getName()) {
+		case VARCHAR:
 			return row.getString(colName);
-		case UUID: 
+		case UUID:
 			return row.getUUID(colName);
-		case VARINT: 
+		case VARINT:
 			return row.getVarint(colName);
-		case BIGINT: 
+		case BIGINT:
 			return row.getLong(colName);
-		case INT: 
+		case INT:
 			return row.getInt(colName);
-		case FLOAT: 
-			return row.getFloat(colName);	
-		case DOUBLE: 
+		case FLOAT:
+			return row.getFloat(colName);
+		case DOUBLE:
 			return row.getDouble(colName);
-		case BOOLEAN: 
+		case BOOLEAN:
 			return row.getBool(colName);
-		case MAP: 
+		case MAP:
 			return row.getMap(colName, String.class, String.class);
-		default: 
+		default:
 			return null;
 		}
 	}
